7e45151d39037a3af252b538b1afd6cfb4ded79c
Chris <post@christoph-schmalfuss.de>
2021-02-20 09:32:32 +0100

    New feature: import of search list from discogs

--- stingy_disc.py
+++ stingy_disc.py
@@ -4,160 +4,157 @@ from bs4 import BeautifulSoup
 from simple_term_menu import TerminalMenu
 from colorama import init, Fore, Back, Style
 
+YOUR_USERNAME = ""
 
 init() #initializing colorama
 
-def read_fav_file():
-	try: 
-		fav_file = open('favorites.txt','r')
-
-	except IOError:
-		print("You have no favorites so far. Use a new search-term to add some...")
-
-	finally: 
-		fav_file.close()
-
-
-def import_favs_from_file():
-
-	try: 
-		global fav_file
-		fav_file = open('favorites.txt','r')
-
-	except FileNotFoundError:
-		open('favorites.txt','w')
+def import_favs_from_discogs():
 
 	global favs
-
-	favs = list(fav_file.read().split(","))
-
-
-def save_request_to_favorites():
-
-	if search_term in favs:
-		print(f'"{search_term}" ist bereits in deinen Favoriten.')
-
-	else: 
-		
-		fav_file = open('favorites.txt','a')
-		fav_file.write(f',{search_term}')
-
-		print(f'Okay. I added >> {search_term} << to your favorites!')
-	
-	main_menu()
-
+	favs = favs_from_discogs
 
 def set_value_color(value):
 
 		global color
+	
+		if value < 15:
+			color=Back.LIGHTGREEN_EX
 
-		if value < 10:
-			color=Fore.GREEN
+		if value >= 15 and value < 20:
+			color=Back.YELLOW
 
-		elif value > 10 and value<20:
-			color=Fore.YELLOW
+		if value >= 20:
+			color=Back.LIGHTRED_EX
 
-		elif value > 20:
-			color=Fore.RED
-
+all_best_prices = []
 
 def get_results(search_term):
 
-	read_fav_file()
+	URL = f'https://www.discogs.com/sell/list?&limit=25&currency=EUR&q={search_term}&format=Vinyl&format_desc=LP'
+	page = requests.get(URL)
+	soup = BeautifulSoup(page.content, 'html.parser')
 
-	URL = f'https://www.discogs.com/sell/list?&limit=250&q={search_term}&format=Vinyl&format_desc=LP'
+	try:
+
+		results = soup.find("table", attrs={"class": "table_block mpitems push_down table_responsive"})
+		results = list(results.tbody.find_all("tr"))  
+
+	except AttributeError:
+		return
+
+	quantity_of_results = len(results)
+                         
+	if len(results) < 3:
+		return                        
+
+	i = quantity_of_results-1
+
+	while i>0:
+		if "Unavailable" in str(results[i]):
+			results.pop(i)
+		i-=1
+
+	obj_list = []
+
+	class Item():
+
+		def __init__(self, item_name, item_price, item_url):
+
+			self.item_name = item_name 
+			self.item_price = item_price
+			self.item_url = item_url
+
+	i = 0
+
+	
+
+	for _ in results:
+
+		regex_for_price = re.findall("(?<=€)(.*)(?=total)",str(results[i]))[0][:-6]
+		regex_for_name = re.findall("(?<=>)(.*)(?=LP)",str(results[i]))[0][:-6]
+		regex_for_url = re.findall("(?<=title)(.*)(?=\")",str(results[i]))[0][31:]
+
+		try: regex_for_name=regex_for_name[:40]
+
+		except IndexError:
+			pass
+
+		try: regex_for_price = float(regex_for_price)
+
+		except ValueError:
+			regex_for_price = 99999
+			pass
+
+		complete_url = "discogs.com"+regex_for_url
+	
+		obj = Item(regex_for_name,regex_for_price,complete_url)
+		obj_list.append(obj)
+		
+		i+=1
+
+	
+	obj_list.sort(key=lambda x: x.item_price, reverse=False)
+
+	if mm_result == 0:
+		for _ in obj_list:
+			set_value_color(_.item_price)
+			print(color,str(_.item_price).ljust(6),_.item_name,Style.RESET_ALL)
+
+	all_best_prices.append(obj_list[0])
+
+	set_value_color(obj_list[0].item_price)
+
+	# Print best result
+	print(f'{color}{str(obj_list[0].item_price).ljust(6)} {obj_list[0].item_name} {obj_list[0].item_url}{Style.RESET_ALL}')
+
+def read_favs_from_discogs():
+	URL = f'https://www.discogs.com/de/wantlist?page=1&limit=25&user={YOUR_USERNAME}'
 	page = requests.get(URL)
 
 	soup = BeautifulSoup(page.content, 'html.parser')
-	results = soup.find(id="pjax_container")
+	release_data = soup.find_all('span',attrs={'class':'release_title set_height'})
 
-	try:	
-		prices = results.find_all(class_='item_description_title')
-		items = results.find_all(class_="item_price hide_mobile")
+	global favs_from_discogs
 
-	except AttributeError:
+	favs_from_discogs = []
 
-		pass 
+	class Item():
 
+			def __init__(self, artist_name, album_name, label_name):
 
-	items_list_clean = re.findall("(?<=- )(.*)(?=\()",results.text)
+				self.artist_name = artist_name
+				self.album_name = album_name
+				self.label_name = label_name
 
-	try:
-		price_list_clean=re.findall("(?<=€)(.*)(?= total)",results.text)
-	except ValueError:
-		price = 0	
+	item_list = []
 
-
-	global results_dict
-
-	results_dict = dict(zip(price_list_clean,items_list_clean))
-
-	# Create two lists from dictionary
-
-	prices = []
-	items = []
-
-	for key in sorted(results_dict,reverse=True):
+	for _ in release_data:
 	
-	 	try:
-	 		if float(key) <= 99:
+		item_data_string = str(_.text.replace("\n","").replace("  ",""))
 
-		 		prices.append(float(key))
-		 		items.append(results_dict[key][:40])
+		obj = Item(re.findall(".+?(?=-)",item_data_string)[0],re.findall("(?<=-).+?(?=\()",item_data_string)[0],re.findall("(?<=\().+?(?=-)",item_data_string)[0])
 
-	 	except ValueError:
-	 		pass					
-	
-
-
-	if mm_result == 0:
-
-		print(f'\n \n Results for "{search_term}":')
-		i = 0
-		for _ in prices:
-
-			set_value_color(prices[i])
-
-			result_string = str(color+str(prices[i]).ljust(5,"0")+" € "+Style.RESET_ALL+str(items[i]))		
-
-			print(result_string)
-
-			i+=1
-
-		print("Add last search_term to favorites?")
-
-		m = TerminalMenu(["Maybe later...","Yes!"])
-		r = m.show()
-
-		if r == 1:
-			save_request_to_favorites()
-			print("added!")
+		if obj.album_name not in item_list:
+			item_list.append(obj)
 		
-		main_menu()
 
-
-	if mm_result == 2: # This only shows the lowest price (for the case that user checks multiple items at once)
-
-		set_value_color(float(prices[len(prices)-1]))
-		print(color,str(prices[len(prices)-1]).ljust(5," "),Style.RESET_ALL,str(items[len(items)-1])[:40]) 
-
+	print("Hi!",len(item_list)," items sucessfully imported from your discogs profile!")
+	favs_from_discogs = item_list
 
 def print_favs():
 
-	print("\n\n These are your saved search_term requests: \n")
-	print(*favs,sep="\n")
+	for number, item in enumerate(favs_from_discogs):
+		print(number, item.artist_name, item.album_name)
 
-	main_menu()
 
-	
 def get_prices_of_all_favorites():
 
-	print(f'Now checking {len(favs)} favorites...')
+	print(f'\n Finding best offers for {len(favs)} favorites...')
 
 	i = len(favs)-1
 	for _ in favs:
 		try:
-			get_results(favs[i])
+			get_results(favs[i].album_name+favs[i].label_name+favs[i].artist_name)
 			i-=1
 		except UnboundLocalError:
 			pass
@@ -169,25 +166,36 @@ def new_search_term():
 
 	get_results(search_term)
 
+def print_all_best_prices():
+	all_best_prices.sort(key=lambda x: x.item_price, reverse=True)
+
+	print("\n \n /// Summary: ///")
+
+	for item in all_best_prices:
+		set_value_color(item.item_price)
+		print(color,str(item.item_price).ljust(6), item.item_name,Style.RESET_ALL)
 
 def main_menu():
-    m = TerminalMenu(["Search for a new item","Show my saved search-terms","Check all prices","Quit"],"\n")
+    m = TerminalMenu(["NEW CUSTOM SEARCH (Beta)","SHOW FAVORITES","CHECK ALL PRICES","QUIT"],"\n")
     global mm_result
     mm_result = m.show()
 
     if mm_result == 0:
     	new_search_term()
 
+
     elif mm_result == 1:
     	print_favs()
 
     elif mm_result == 2:
     	get_prices_of_all_favorites()
+    	print_all_best_prices()
 
     else:
     	exit()
 
 
-import_favs_from_file()
+read_favs_from_discogs()
+import_favs_from_discogs()
 main_menu()
 
